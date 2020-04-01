6322f615cf4a599ee36fc3d340884594bd607ab8
프로그램동산 programgarden <59354057+programgarden@users.noreply.github.com>
2020-04-01 14:38:54 +0900

    Add files via upload

---
+++ 실전코드/__init__.py
@@ -0,0 +1,15 @@
+from kiwoom.kiwoom import *
+import sys
+from PyQt5.QtWidgets import *
+
+class Main():
+    def __init__(self):
+        print("Main() start")
+
+        self.app = QApplication(sys.argv) # PyQt5로 실행할 파일명을 자동 설정
+        self.kiwoom = Kiwoom() # 키움 클래스 실행
+        self.app.exec_() # 이벤트 루프 실행
+
+
+if __name__ == "__main__":
+    Main()
